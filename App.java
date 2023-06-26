32974455d43b56b6398ae521b92214af72d43cf7
owaisazmal <87142612+owaisazmal@users.noreply.github.com>
2023-06-25 19:55:27 -0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,49 @@
+import tools.*;
+
+public class App
+{    
+    public static final char getAscii(int code) 
+    {
+            return Symbols.EXTENDED[code];
+    }
+
+    public static final void printChar(int ascii) 
+    {
+        System.out.printf("%c%n", getAscii(ascii) );
+    }
+
+    public static void cls()
+    {
+                // cls
+        System.out.print("\033[H\033[2J");  
+        System.out.flush(); 
+    }
+    
+    public static void printxy(int col, int row, String val)
+    {
+        char escCode = 0x1B;
+        System.out.print(String.format("%s[%d;%df",escCode,row,col));
+        System.out.print(val);
+    }
+    
+    public static void Box(int x, int y, int w, int h, int ascii, String fcolor,String bcolor)
+    {
+        char val = getAscii(ascii);
+        for (int i=0; i < w; i++)
+        {
+            for (int j=0; j< h; j++)
+            {
+                
+                printxy(x+i,y+j,fcolor + bcolor + val);
+            }
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        CardGame C = new CardGame();
+        C.Play();
+
+     while (true);
+    }
+}
---
+++ sample.java
@@ -0,0 +1,8 @@
+public class sample {
+    public static void main(String[] args) throws InterruptedException {
+        playBeep();
+    }
+    public static void playBeep() {
+        System.out.print("\u0007"); // Prints the ASCII bell character
+    }
+}
